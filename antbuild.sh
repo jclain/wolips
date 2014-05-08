42ad204fc9af17853df44c237c7e549e50831e98
Jephte CLAIN <Jephte.Clain@gmail.com>
2014-05-08 22:25:19 +0400

    ajouter un suffixe pour l'ur

--- antbuild.sh
+++ antbuild.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # -*- coding: utf-8 mode: sh -*- vim:sw=4:sts=4:et:ai:si:sta:fenc=utf-8
 
+SUFFIX=ur1
+DATE="$(date +%Y%m%d)"
+JAVA_HOME=~/opt/jvm64/sun-jdk-1.5
+
+################################################################################
+export JAVA_HOME
 cd "$(dirname "$0")"
-
-export JAVA_HOME=~/opt/jvm64/sun-jdk-1.5
-ant -Dbuild.version="3.7.$(date +%Y%m%d)" "$@"
-
+ant -Dbuild.version="3.7.$DATE$SUFFIX" "$@"
