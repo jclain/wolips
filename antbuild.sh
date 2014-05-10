c192eb3a15511e038e660fddd588f13929caa3ed
Jephte CLAIN <Jephte.Clain@gmail.com>
2014-05-10 09:24:02 +0400

    déploiement après build

--- antbuild.sh
+++ antbuild.sh
@@ -7,4 +7,5 @@ JAVA_HOME=~/opt/jvm64/sun-jdk-1.5
 ################################################################################
 export JAVA_HOME
 cd "$(dirname "$0")"
-ant -Dbuild.version="3.7.$(date +%Y%m%d).$ITER" "$@"
+ant -Dbuild.version="3.7.$(date +%Y%m%d).$ITER" "$@" || exit 1
+rsync -av --delete temp/dist/ ../WOLips37UR-site
