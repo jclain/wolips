4edbee29d5eda6c295d3f99ad179a52eaa531cee
Jephte CLAIN <Jephte.Clain@univ-reunion.fr>
2014-05-30 09:00:26 +0400

    spécifier le chemin vers eclipse

--- antbuild.sh
+++ antbuild.sh
@@ -3,9 +3,10 @@
 
 ITER=1
 JAVA_HOME=~/opt/jvm64/sun-jdk-1.5
+ECLIPSE_HOME=~/opt/sdk_wolips_37/eclipse64/eclipse
 
 ################################################################################
 export JAVA_HOME
 cd "$(dirname "$0")"
-ant -Dbuild.version="3.7.$(date +%Y%m%d).$ITER" "$@" || exit 1
+ant -Dbuild.version="3.7.$(date +%Y%m%d).$ITER" -Declipse.home="$ECLIPSE_HOME" "$@" || exit 1
 rsync -av --delete temp/dist/ ../WOLips37UR-site
